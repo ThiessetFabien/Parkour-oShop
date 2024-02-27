d669cdf81f27a40fda9b1d747d99ac0857576b3e
Fabien Thiesset <thiessetfabien3@gmail.com>
2024-02-27 09:18:34 +0100

    initial commit

--- middlewares/loadUserToLocals.js
+++ middlewares/loadUserToLocals.js
@@ -1,12 +1,12 @@
 // On se sert des locals pour pouvoir utiliser la variable user dans les views.
 const loadUserToLocals = (req, res, next) => {
-  if (req.session.user) {
-    res.locals.user = req.session.user;
-  } else {
-    res.locals.user = null;
-  }
+    if (req.session.user) {
+        res.locals.user = req.session.user;
+    } else {
+        res.locals.user = null;
+    }
 
-  next();
+    next();
 };
 
 module.exports = loadUserToLocals;
