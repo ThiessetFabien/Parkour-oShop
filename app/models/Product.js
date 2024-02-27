ea57fff126e14e7d755b20be7a3f2900dafef35b
Fabien Thiesset <thiessetfabien3@gmail.com>
2024-02-27 09:31:53 +0100

    feet: model product

--- app/models/Product.js
+++ app/models/Product.js
@@ -2,15 +2,43 @@ const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../database');
 
 class Product extends Sequelize.Model {}
-/**
- * Voici les champs nécessaires pour faire le Model
- * category_id int
- * ref string
- * image string
- * title string
- * description text
- * price number
- * tableName: 'products',
- */
 
-module.exports = Product;
+Product.init({
+    category_id: {
+        type: DataTypes.INTEGER,
+    },
+    ref: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    sequelize,
+    tableName: 'products',
+}),
+    /**
+     * Voici les champs nécessaires pour faire le Model
+     * category_id int
+     * ref string
+     * image string
+     * title string
+     * description text
+     * price number
+     * tableName: 'products',
+     */
+
+    (module.exports = Product);
