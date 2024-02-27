51814837b77bcd9eb084795790b3e0fe1f41a1ff
Fabien Thiesset <thiessetfabien3@gmail.com>
2024-02-27 15:47:42 +0100

    feet/signup

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -10,13 +10,38 @@ const userController = {
     register: async (req, res) => {
         try {
             // !! votre code à partir d'ici
+            const { firstname, lastname, email, password, passwordConfirm } =
+                req.body;
             // verifier l'email avec le package npm email-validator
+            // console.log('email validator ', emailValidator.validate(email));
+            if (!emailValidator.validate(email)) {
+                return res.render('register', {
+                    errorMessage: 'Email invalide',
+                });
+            }
 
             // verifier si password correspond à password confirm
+            console.log('mdp identiques ', password !== passwordConfirm);
+            if (password !== passwordConfirm) {
+                return res.render('register', {
+                    errorMessage: 'Les mots de passe ne correspondent pas',
+                });
+            }
 
             // hash password
+            const salt = await bcrypt.genSalt(15);
+            // console.log('salt ', salt);
+            const hash = await bcrypt.hash(password, salt);
+            // console.log('hash ', hash);
 
             // attribuer un rôle ici, le role customer.
+
+            await User.create({
+                name: `${firstname} ${lastname}`,
+                email,
+                password: hash,
+                role_id: 1,
+            });
 
             // sauvegarder user
 
